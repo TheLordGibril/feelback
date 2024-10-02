1303996378db90c934925bcd57d4f571b0b8a3a5
TheLordGibril <gabidule03@gmail.com>
2024-10-02 13:03:41 +0200

    final model

---
+++ backend/feelback/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    lane_number = models.CharField(max_length=255)
+    address = models.TextField()
+    zip_code = models.CharField(max_length=20)
+    town = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class Deliverer(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class Package(models.Model):
+    STATES = [
+        ('EXCELLENT', 'Excellent'),
+        ('TRES_BON_ETAT', 'Très bon état'),
+        ('ETAT_CORRECT', 'État correct'),
+        ('ENDOMMAGE', 'Endommagé'),
+        ('TRES_ENDOMMAGE', 'Très endommagé')
+    ]
+    departure_state = models.CharField(max_length=14, choices=STATES)
+    arrival_state = models.CharField(max_length=14, choices=STATES)
+    weight = models.FloatField()
+    fragile = models.BooleanField(default=False)
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class Delivery(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    deliverer = models.ForeignKey(Deliverer, on_delete=models.CASCADE)
+    scheduled_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class Form(models.Model):
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
+
+
+class FormQuestion(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.IntegerField(
+        validators=[
+            MaxValueValidator(5), MinValueValidator(1)]
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def str(self):
+        return
+
+    def unicode(self):
+        return
