8fac655bee1fbedf08478822badf997e8a21fa26
Antoine Cerdan <eosiswastaken@gmail.com>
2024-10-03 20:57:52 +0200

    POST request

--- front-end/src/components/radiogroup.jsx
+++ front-end/src/components/radiogroup.jsx
@@ -1,21 +1,25 @@
 import "./radiogroup.css"
 
 export default function RadioGroup(props) {
+    console.log("a")
 
+    const handleLocalChange = (event) => {
+        props.handleChange(props.name,event.target.id)
+    };
 
     return(
         <div className="container-radiogroup">
             <p id="title-radiogroup">{props.text}</p>
             <div id="container-radio">
-                <input type="radio" name="1" id="1" />
+                <input type="radio" name={props.name} id="1" onChange={handleLocalChange}/>
                 <label htmlFor="1">1</label>
-                <input type="radio" name="2" id="2" />
+                <input type="radio" name={props.name} id="2" onChange={handleLocalChange} />
                 <label htmlFor="1">2</label>
-                <input type="radio" name="3" id="3" />
+                <input type="radio" name={props.name} id="3" onChange={handleLocalChange} />
                 <label htmlFor="1">3</label>
-                <input type="radio" name="4" id="4" />            
+                <input type="radio" name={props.name} id="4" onChange={handleLocalChange} />            
                 <label htmlFor="1">4</label>
-                <input type="radio" name="5" id="5" />
+                <input type="radio" name={props.name} id="5" onChange={handleLocalChange} />
                 <label htmlFor="1">5</label>
             </div>
         </div>
--- front-end/src/routes/form.jsx
+++ front-end/src/routes/form.jsx
@@ -4,55 +4,71 @@ import RadioGroup from "../components/radiogroup";
 import Header from "../components/header"
 import Footer from "../components/footer"
 
+import React from "react";
 import axios from "axios";
 
 import { Link } from "react-router-dom";
 
 export default function Form() {
 
-    
-async function post_result() {
-  try {
-    const response = await axios.post('/api/create-answer/', [
-      {
-        form: 1,
-        question: 1,
-        answer: 4,
-        customer: 2,
-        deleted_at: null
-      },
-      {
-        form: 1,
-        question: 2,
-        answer: 5,
-        customer: 2,
-        deleted_at: null
-      },
-      {
-        form: 1,
-        question: 3,
-        answer: 5,
-        customer: 2,
-        deleted_at: null
-      }
-    ]);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-}
+    const [formState, setFormState] = React.useState({
+        delay: 3,
+        state: 3,
+        behavior: 3
+    });
 
+    function test(name, value) {
+        setFormState(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    
+        console.log(name, value); 
+    }
+    
+
+    async function post_result() {
+        try {
+            const response = await axios.post('/api/create-answer/', [
+                {
+                    form: 1,
+                    question: 1,
+                    answer: formState.delay,
+                    customer: 3,
+                    deleted_at: null
+                },
+                {
+                    form: 1,
+                    question: 2,
+                    answer: formState.state,
+                    customer: 3,
+                    deleted_at: null
+                },
+                {
+                    form: 1,
+                    question: 3,
+                    answer: formState.behavior,
+                    customer: 3,
+                    deleted_at: null
+                }
+            ]);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
 
 
     return(
         <div className="container-form">
             <Header/>
-            <Link to="/" id="home-button" className="link-button">revenir a la page d'accueil</Link>
+            <Link to="/" id="home-button" className="link-button">revenir a la page daccueil</Link>
             <h2>Donner votre avis</h2>
         
-            <RadioGroup text="évaluer de 1 a 5 le respect du délai de livraison"/>
-            <RadioGroup text="évaluer de 1 a 5 l'état de votre colis a sa réception"/>
-            <RadioGroup text="évaluer de 1 a 5 le comportement du livreur"/>
+            <RadioGroup handleChange={test} text="évaluer de 1 a 5 le respect du délai de livraison" name="delay"/>
+            <RadioGroup handleChange={test} text="évaluer de 1 a 5 l'état de votre colis a sa réception" name="state"/>
+            <RadioGroup handleChange={test} text="évaluer de 1 a 5 le comportement du livreur" name="behavior"/>
 
             <button id="send-button" className="link-button" onClick={post_result}>envoyer</button>
             <Footer/>
